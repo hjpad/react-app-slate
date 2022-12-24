ce93e8edb6363e1263264b4a5c8ddc36f567d448
hjpad <116934207+hjpad@users.noreply.github.com>
2022-12-24 17:41:39 +1000

    serialize and deserialize plain text values

--- src/App.js
+++ src/App.js
@@ -7,24 +7,43 @@ import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 import { Editor, Transforms, Text } from 'slate';
 
-// const initialValue = [
-//   {
-//     type: 'paragraph',
-//     children: [{ text: 'A line of text in a paragraph.' }],
-//   },
-// ];
+const initialValue0 = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+];
+
+// Import the `Node` helper interface from Slate.
+import { Node } from 'slate'
+
+// Define a serializing function that takes a value and returns a string.
+const serialize = value => {
+  return (
+    value
+      // Return the string content of each paragraph in the value's children.
+      .map(n => Node.string(n))
+      // Join them all with line breaks denoting paragraphs.
+      .join('\n')
+  )
+}
+
+// Define a deserializing function that takes a string and returns a value.
+const deserialize = string => {
+  // Return a value array of children derived by splitting the string.
+  return string.split('\n').map(line => {
+    return {
+      children: [{ text: line }],
+    }
+  })
+}
 
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()))
-  // Update the initial content to be pulled from Local Storage if it exists.
+  // Use our deserializing function to read the data from Local Storage.
   const initialValue = useMemo(
     () =>
-      JSON.parse(localStorage.getItem('content')) || [
-        {
-          type: 'paragraph',
-          children: [{ text: 'A line of text in a paragraph.' }],
-        },
-      ],
+    deserialize(localStorage.getItem('content')) || '',
     []
   )
 
@@ -37,9 +56,8 @@ const App = () => {
           op => 'set_selection' !== op.type
         )
         if (isAstChange) {
-          // Save the value to Local Storage.
-          const content = JSON.stringify(value)
-          localStorage.setItem('content', content)
+          // Serialize the value and save the string value to Local Storage.
+          localStorage.setItem('content', serialize(value))
         }
       }}
     >
