ab43e7b9e2b9a0c4f465d04e34092e77df068b02
hjpad <116934207+hjpad@users.noreply.github.com>
2022-12-24 10:52:59 +1000

    Defining Custom Elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import React from "react";
 // import "./style.css";
 
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
+import { Editor, Transforms } from 'slate';
 
 const initialValue = [
   {
@@ -15,21 +17,50 @@ const initialValue = [
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   return (
     <Slate editor={editor} value={initialValue}>
       <Editable
-        // Define a new handler which prints the key that was pressed.
+        renderElement={renderElement}
         onKeyDown={(event) => {
-          if (event.key === '&') {
-            // Prevent the ampersand character from being inserted.
-            event.preventDefault()
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText('and')
+          if (event.key === '`' && event.ctrlKey) {
+            // Prevent the "`" from being inserted
+            event.preventDefault();
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === 'code',
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              { match: (n) => Editor.isBlock(editor, n) }
+            );
           }
         }}
       />
     </Slate>
   );
+};
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
 };
 
 export default App;
