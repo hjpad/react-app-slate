8db4d75bce2e2d2d41f861ca6b38f2f8bf30e09c
hjpad <116934207+hjpad@users.noreply.github.com>
2022-12-24 16:43:57 +1000

    Executing Commands

--- src/App.js
+++ src/App.js
@@ -14,6 +14,44 @@ const initialValue = [
   },
 ];
 
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.bold === true,
+      universal: true,
+    });
+
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === 'code',
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
+};
+
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
@@ -26,16 +64,34 @@ const App = () => {
     }
   }, []);
 
-  // Define a leaf rendering function that is memoized with `useCallback`.
   const renderLeaf = useCallback((props) => {
     return <Leaf {...props} />;
   }, []);
 
   return (
+    // Add a toolbar with buttons that call the same methods.
     <Slate editor={editor} value={initialValue}>
+      <div>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
       <Editable
+        editor={editor}
         renderElement={renderElement}
-        // Pass in the `renderLeaf` function.
         renderLeaf={renderLeaf}
         onKeyDown={(event) => {
           if (!event.ctrlKey) {
@@ -45,35 +101,13 @@ const App = () => {
           switch (event.key) {
             case '`': {
               event.preventDefault();
-              const [match] = Editor.nodes(editor, {
-                match: (n) => n.type === 'code',
-              });
-              Transforms.setNodes(
-                editor,
-                { type: match ? null : 'code' },
-                { match: (n) => Editor.isBlock(editor, n) }
-              );
+              CustomEditor.toggleCodeBlock(editor);
               break;
             }
 
             case 'b': {
               event.preventDefault();
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                { match: (n) => Text.isText(n), split: true }
-              );
-              break;
-            }
-
-            case ' ': {
-              console.log('a');
-              event.preventDefault();
-              Transforms.setNodes(
-                editor,
-                { bold: false },
-                { match: (n) => Text.isText(n), split: true }
-              );
+              CustomEditor.toggleBoldMark(editor);
               break;
             }
           }
